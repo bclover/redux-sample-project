3f80a50b45e3070c824238d279c723db63106995
Bryan Clover <bclover09@gmail.com>
2016-09-14 14:47:52 -0400

    Section 4 progress checkin.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Bad Parts' },
+    { title: 'Heavy Metal: Mental Health' },
+    { title: 'Your AR-15: And How to Use It' },
+    { title: 'Politicians: And How to Cope' },
+    { title: '2016: Why Are There No Good Choices?' }
+  ]
+}
