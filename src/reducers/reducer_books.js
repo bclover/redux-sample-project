0f7189ea865c139624022b3544d9dfdf38223574
Bryan Clover <bclover09@gmail.com>
2016-09-15 22:53:45 -0400

    Finished Section 4.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    { title: 'Javascript: The Bad Parts' },
-    { title: 'Heavy Metal: Mental Health' },
-    { title: 'Your AR-15: And How to Use It' },
-    { title: 'Politicians: And How to Cope' },
-    { title: '2016: Why Are There No Good Choices?' }
+    { title: 'Javascript: The Bad Parts', pages: 535 },
+    { title: 'Heavy Metal: Mental Health', pages: 142 },
+    { title: 'Your AR-15: And How to Use It', pages: 260 },
+    { title: 'Politicians: And How to Cope', pages: 119 },
+    { title: '2016: Why Are There No Good Choices?',  pages: 788 }
   ]
 }
